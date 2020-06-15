6381ae3213392808083b8de33cb3f6e8d26174cb
mpiriya <piriya.matt@gmail.com>
2020-06-15 17:11:24 -0400

    using csv library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,10 +2,22 @@ require "csv"
 
 puts "EventManager Initialized!"
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+# lines = File.readlines "event_attendees.csv"
+# lines.each_with_index do |line, index|
+#   next if index == 0
+#   columns = line.split(",")
+#   name = columns[2]
+#   puts name
+# end
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
 end
